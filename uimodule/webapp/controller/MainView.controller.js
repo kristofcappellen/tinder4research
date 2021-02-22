354678a88fdc494e628fd7f84feff77ea558a4ec
Kristof Cappellen <kristof.cappellen@flexso.com>
2021-02-22 11:34:32 +0100

    initial commit

--- uimodule/webapp/controller/MainView.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-  "flexso/research/tinder4research/controller/BaseController"
-], function(Controller) {
-  "use strict";
-
-  return Controller.extend("flexso.research.tinder4research.controller.MainView", {});
-});
